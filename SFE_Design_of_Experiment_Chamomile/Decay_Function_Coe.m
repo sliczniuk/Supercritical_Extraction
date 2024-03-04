167f9a44a41670fb5452db17d86f669d8f1dc12b
Sliczniuk Oliwer <oliwer.sliczniuk@aalto.fi>
2024-03-04 13:13:47 +0200

    04.03

--- SFE_Design_of_Experiment_Chamomile/Decay_Function_Coe.m
+++ SFE_Design_of_Experiment_Chamomile/Decay_Function_Coe.m
@@ -1,8 +1,10 @@
 function [Upsilon] = Decay_Function_Coe(Re, F, parameters)
 
-    a = parameters{46};
-    b = parameters{47};
+    a = parameters{47};
+    b = parameters{48};
+    c = parameters{49};
 
-    Upsilon  = 4.717 + 11.012 .* Re - 0.876 .* (F * 10^5);
+    Upsilon  = a + b .* Re + c .* (F * 10^5);
+    %Upsilon  = 4.717 + 11.012 .* Re - 0.876 .* (F * 10^5);
 
 end
